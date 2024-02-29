4a17ad99ae509be6a3acfd78ca734f087f73bbb9
mariadv_ <95478604+mariaduva@users.noreply.github.com>
2024-02-29 21:32:05 +0100

    labs 1-3 **missing labs mods **

---
+++ FC2practicasWS/pr1_lab/pr1_lab.asm
@@ -0,0 +1,41 @@
+/*---------------------------------------------------------------------
+**
+**  FICHERO:
+**    pr1_a.asm
+**
+**  C++:
+**	# define N 10
+**	int res = 0;
+**	for (int i = 0; i < N ; i ++) {
+**		res += i ;
+**	}
+**
+**-------------------------------------------------------------------*/
+.global main
+.equ N, 10
+
+.bss
+	res1: 	.space 4
+	res2: 	.space 4
+
+.text
+main:
+	li s1,N // s1 es n
+	li t0,0 // t0 es i
+	li t1,0 // t1 es res1
+	li t2,0 // t2 es res2
+for:
+	bge t0,s1,efor
+	add t1,t1,t0 // res1 += i ;
+if:
+
+eif:
+	add t0,t0,1 // i++;
+	j for
+efor:
+	la t2, res1
+	sw t1, 0(t2)
+	j .
+.end
+
+
